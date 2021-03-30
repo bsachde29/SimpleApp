247dc20588a9fb3a44570e1762a7b71189718304
Abhi Sharma <sharm395@purdue.edu>
2021-03-30 02:39:28 -0400

    Displayed sample products

---
+++ app/src/main/java/com/example/simpleapp/ProductCell.java
@@ -0,0 +1,66 @@
+package com.example.simpleapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class ProductCell {
+
+    TableRow tableRow;
+    LinearLayout linearLayoutH, linearLayoutV;
+    ImageView imageView;
+    TextView title, desc, price;
+
+    ProductCell(Context ctx, String Title, String Desc, String Price, Drawable d) {
+        tableRow = new TableRow(ctx);
+        linearLayoutH = new LinearLayout(ctx);
+        linearLayoutV = new LinearLayout(ctx);
+        title = new TextView(ctx);
+        desc = new TextView(ctx);
+        price = new TextView(ctx);
+        imageView = new ImageView(ctx);
+        title.setText(Title);
+        price.setText("$ " + Price);
+        desc.setText(Desc);
+        desc.setMaxWidth(400);
+
+
+        imageView.setImageDrawable(d);
+
+
+        setMargins((ViewGroup) tableRow, 10, 20, 8, 8);
+        tableRow.setBackgroundColor(Color.parseColor("#ffffff"));
+        tableRow.setPadding(3, 30, 3, 30);
+        desc.setPadding(5, 30, 5, 0);
+
+        linearLayoutH.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayoutV.setOrientation(LinearLayout.VERTICAL);
+        linearLayoutH.setMinimumHeight(100);
+        linearLayoutH.addView(imageView);
+        linearLayoutV.addView(title, 0);
+        linearLayoutV.addView(desc, 1);
+        tableRow.addView(linearLayoutH);
+        tableRow.addView(linearLayoutV);
+        tableRow.addView(price);
+
+    }
+
+    public void setMargins(View view, int left, int top, int right, int bottom) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            view.requestLayout();
+        }
+    }
+
+
+}
+
--- app/src/main/java/com/example/simpleapp/ProductListActivity.java
+++ app/src/main/java/com/example/simpleapp/ProductListActivity.java
@@ -2,13 +2,41 @@ package com.example.simpleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+
+import java.io.InputStream;
 
 public class ProductListActivity extends AppCompatActivity {
+
+    TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+
+        ProductCell cell1 = null, cell2 = null;
+        try {
+            InputStream ims = getAssets().open("prod_holder.jpg");
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            ims .close();
+            cell1 = new ProductCell(getApplicationContext(), "Arjun", "Shivam loda loda loda loda loda loda loda loda loda loda ", "2", d);
+            cell2 = new ProductCell(getApplicationContext(), "gfxchjk", "dbvck hbsdhjv ljhgsadfb hkvbh kas dbkhv cbhk adsbhj ", "3000", d);
+
+
+            tableLayout = findViewById(R.id.tableLayoutProdList);
+            tableLayout.addView(new RowDivider(getApplicationContext()).tableRow);
+            tableLayout.addView(cell1.tableRow);
+            tableLayout.addView(new RowDivider(getApplicationContext()).tableRow);
+            tableLayout.addView(cell2.tableRow);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
