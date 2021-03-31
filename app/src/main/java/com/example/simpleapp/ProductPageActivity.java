d6ba52a9c91d40910a74a841c3cb7bf9ce80c7e0
Abhi Sharma <sharm395@purdue.edu>
2021-03-31 03:05:11 -0400

    ProductPageActivity : set up and initialized instance variables

--- app/src/main/java/com/example/simpleapp/ProductPageActivity.java
+++ app/src/main/java/com/example/simpleapp/ProductPageActivity.java
@@ -3,8 +3,15 @@ package com.example.simpleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProductPageActivity extends AppCompatActivity {
+
+    TextView title = findViewById(R.id.title_pp), desc = findViewById(R.id.desc_pp), price = findViewById(R.id.price_pp);
+    Button button = findViewById(R.id.add_pp);
+    ImageView image = findViewById(R.id.imageView_pp);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
