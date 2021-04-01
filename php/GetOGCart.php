d2106f70118d83c07df11cf48c09d0b82000806a
arjunharbhajanka <aharbhaj@purdue.edu>
2021-04-01 03:07:50 -0400

    OGCart php made

--- php/GetOGCart.php
+++ php/GetOGCart.php
@@ -13,21 +13,12 @@ $dbname = "simpledb";
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM Cart_Product_Count WHERE CartID = '$cartID'");
+    $stmt = $conn->prepare("SELECT * FROM Cart WHERE CartID = '$cartID'");
     $stmt->execute();
     $cart = $stmt->fetchAll(PDO::FETCH_ASSOC);
     //echo $cart[0];
-    $cartObj = $cart;
-//    $prodArray = array();
-//    $counter = 0;
-//    foreach ($cart[$counter]['ProductID'] as $id) {
-//        // echo "$id \n";
-//        $stmt2 = $conn->prepare("SELECT * FROM Product WHERE ProductID = '$id' AND isSubProduct = 0");
-//        $stmt2->execute();
-//        $prodList = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-//        $prodArray[$counter] = $prodList;
-//        $counter++;
-//    }
+    $cartObj = $cart[0];
+
     echo json_encode($cartObj);
 
 }
