859cff51497c5c84b1c6e96be786f200d98b1b20
Abhi Sharma <sharm395@purdue.edu>
2021-04-02 05:26:22 -0400

    Error corrected

--- app/src/main/java/com/example/simpleapp/AddressBookActivity.java
+++ app/src/main/java/com/example/simpleapp/AddressBookActivity.java
@@ -24,7 +24,7 @@ import java.util.Map;
 public class AddressBookActivity extends AppCompatActivity {
 
     TableLayout tableLayout;
-    Button add
+    Button add;
     private String BuyerID;
 
     @Override
