9afb9663d598c7748db9784c14119fc59a363901
arjunharbhajanka <aharbhaj@purdue.edu>
2021-04-20 04:35:49 -0400

    Search Backend Phase1

---
+++ php/Search.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+$searchName = $_GET['searchName'];
+
+$servername = "selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com";
+$username = "simpledb";
+$password = "sell1234";
+$dbname = "simpledb";
+
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM Product WHERE Name like '%{$searchName}%'");
+    $stmt->execute();
+    $order = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($order[0]);
+}
+catch(PDOException$e) {
+    echo "Error: ".$e ->getMessage();
+
+}
+
+?>
