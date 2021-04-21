e416bee5205e54e0772800c4340cc3b456f56d19
arjunharbhajanka <aharbhaj@purdue.edu>
2021-04-21 01:17:00 -0400

    Search Phase 4

--- php/Search.php
+++ php/Search.php
@@ -1,5 +1,6 @@
 <?php
 error_reporting(E_ALL);
+$sellerId = $_POST['SellerID'];
 $searchName = $_POST['searchName'];
 
 $servername = "selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com";
@@ -11,10 +12,21 @@ $dbname = "simpledb";
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM Product WHERE Name like '%{$searchName}%' OR Description like '%{$searchName}%'");
+    $stmt = $conn->prepare("SELECT * FROM Seller_Product WHERE SellerID = '$sellerId'");
     $stmt->execute();
-    $order = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($order);
+    $product_temp = $stmt->fetchAll(PDO::FETCH_COLUMN, 1);
+    $prodArray = array();
+    $counter = 0;
+    foreach ($product_temp as $id) {
+        // echo "$id \n";
+        $stmt2 = $conn->prepare("SELECT * FROM Product WHERE ProductID = '$id' AND (Name like '%{$searchName}%' OR Description like '%{$searchName}%')");
+        $stmt2->execute();
+        $prodList = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        $prodArray[$counter] = $prodList[0];
+        $counter++;
+    }
+    echo json_encode(($prodArray));
+
 }
 catch(PDOException$e) {
     echo "Error: ".$e ->getMessage();
@@ -22,3 +34,42 @@ catch(PDOException$e) {
 }
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!---->
+<?php
+//error_reporting(E_ALL);
+//$searchName = $_POST['searchName'];
+//
+//$servername = "selldb.cqt5tgj7qyws.us-east-2.rds.amazonaws.com";
+//$username = "simpledb";
+//$password = "sell1234";
+//$dbname = "simpledb";
+//
+//
+//try{
+//    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    $stmt = $conn->prepare("SELECT * FROM Product WHERE Name like '%{$searchName}%' OR Description like '%{$searchName}%'");
+//    $stmt->execute();
+//    $order = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//    echo json_encode($order);
+//}
+//catch(PDOException$e) {
+//    echo "Error: ".$e ->getMessage();
+//
+//}
+//
+//?>
