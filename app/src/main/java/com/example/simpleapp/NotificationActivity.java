940231c1afca867059597f613942644457049270
shivambairoliya <shivambairoliya@gmail.com>
2021-04-23 03:15:49 -0400

    Updated PHP

--- app/src/main/java/com/example/simpleapp/NotificationActivity.java
+++ app/src/main/java/com/example/simpleapp/NotificationActivity.java
@@ -3,12 +3,24 @@ package com.example.simpleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        tableLayout = findViewById(R.id.notificationTable);
+
+        addNotifications();
+
+    }
+
+    void addNotifications() {
+        
     }
 }
