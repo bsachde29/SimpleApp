96a3da2f2b4af23c22b79d0cd1365aa957818481
Abhi Sharma <sharm395@purdue.edu>
2021-04-23 04:11:04 -0400

    Notificationsssss

--- app/src/main/java/com/example/simpleapp/NotificationCell.java
+++ app/src/main/java/com/example/simpleapp/NotificationCell.java
@@ -14,7 +14,16 @@ public class NotificationCell {
     NotificationCell(Context context, String notification) {
         textView = new TextView(context);
         textView.setText(notification);
+
+        textView.setMinHeight(300);
+        textView.setMaxWidth(1000);
+
+
+
+        textView.setLines(2);
         tableRow = new TableRow(context);
+        tableRow.setGravity(16);
+        tableRow.setMinimumHeight(300);
         tableRow.addView(textView);
     }
 
